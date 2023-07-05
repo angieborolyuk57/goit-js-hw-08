a0886e04024dd42e2edbcf43a4df5c18d52fcece
angieborolyuk57 <116447706+angieborolyuk57@users.noreply.github.com>
2023-07-05 23:59:03 +0200

    corrections

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,48 @@
-
 var throttle = require('lodash.throttle');
 
 const refs = {
-    email: document.querySelector('input[name="email"]'),
-    message: document.querySelector('textarea[name="message"]'),
-    form: document.querySelector('.feedback-form'),
-  };
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+  form: document.querySelector('.feedback-form'),
+};
 const STORAGE_KEY = 'feedback-form-state';
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.email.addEventListener('input', throttle(onTextInput, 1000));
-refs.message.addEventListener('input', throttle(onTextInput, 1000));
+refs.email.addEventListener('input', throttle(onTextInput, 500));
+refs.message.addEventListener('input', throttle(onTextInput, 500));
 
 populateTextArea();
 
-
 function onFormSubmit(e) {
-e.preventDefault();
-console.log('Sending form');
-e.currentTarget.reset();
-localStorage.removeItem(STORAGE_KEY)
+  e.preventDefault();
+
+  const formData = {
+    email: refs.email.value,
+    message: refs.message.value,
+  };
+
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextInput(e) {
-    const textInput = e.currentTarget.value;
+function onTextInput() {
+  const formData = {
+    email: refs.email.value,
+    message: refs.message.value,
+  };
 
-    localStorage.setItem(STORAGE_KEY, textInput);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateTextArea() {
-    const storedData = localStorage.getItem(STORAGE_KEY);
+  const storedData = localStorage.getItem(STORAGE_KEY);
 
   if (storedData) {
-      console.log('Data is stored');
-      refs.message.value = storedData;
-      refs.email.value = storedData;
+    console.log('Data is stored');
+    const formData = storedData;
+    refs.email.value = formData.email;
+    refs.message.value = formData.message;
+  }
 }
-}
-
-
-
